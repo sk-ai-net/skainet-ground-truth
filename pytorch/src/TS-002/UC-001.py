9d7f93688c99e0b31253bb43f53de0aab3868743
Michal Harakal <michal.harakal@googlemail.com>
2025-05-12 21:49:33 +0200

    Add initial simple use cases for slicing

---
+++ pytorch/src/TS-002/UC-001.py
@@ -0,0 +1,26 @@
+import torch
+from  gt.core import Executable
+
+@Executable("take all rows, take columns starting from index 1 to the end")
+def slice_1():
+    # Create a 3x3 tensor with values from 1 to 9
+    x = torch.arange(1, 10).reshape(3, 3)
+    print(x)
+    y = x[:,1:]
+    return [x], y
+
+@Executable("take rows starting with the index 1, take columns starting from index 1 to the end")
+def slice_2():
+    # Create a 3x3 tensor with values from 1 to 9
+    x = torch.arange(1, 10).reshape(3, 3)
+    print(x)
+    y = x[1:,1:]
+    return [x], y
+
+
+@Executable("Take the rows from the second to the second last and all columns until the second last one")
+def slice_2():
+    # Create a 3x3 tensor with values from 1 to 9
+    x = torch.arange(1, 10).reshape(3, 3)
+    y = x[1:-1,:-2]
+    return [x], y
